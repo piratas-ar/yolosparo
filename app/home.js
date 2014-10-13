4fd9701f6f5a398460e8a4e35e2982da3a481f18
Matías MI <matias.mi@riseup.net>
2014-10-12 21:36:11 -0300

    Display list of legislatives.
    * Created repository to manage legislatives.
    * Created legislative entity.

--- app/home.js
+++ app/home.js
@@ -1,5 +1,17 @@
+var LegislativesRepository = require("../lib/LegislativesRepository");
+
 // Renders the home page.
-app.get('/', function(req, res){
-  res.render("home.html");
+app.get('/', function (req, res) {
+  var repo = new LegislativesRepository(req.db);
+
+  repo.list(function (err, legislatives) {
+    if (err) {
+      req.db.rollback();
+      return;
+    }
+    res.render("home.html", {
+      legislatives: legislatives
+    });
+  });
 });
 
