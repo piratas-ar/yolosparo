1f893f1e7a83b18055eb116c4f3fdb23e2213487
to-more <to_more@mailoo.org>
2014-10-15 04:00:23 -0300

    Modification - added call to function get_by_district

--- app/home.js
+++ app/home.js
@@ -3,16 +3,30 @@ var LegislativesRepository = require("../lib/LegislativesRepository");
 // Renders the home page.
 app.get('/', function (req, res) {
   var repo = new LegislativesRepository(req.db);
+  var district = req.param("district");
 
-  repo.list(function (err, legislatives) {
-    if (err) {
-      req.db.rollback();
-      return;
-    }
-    res.render("home.html", {
-      legislatives: legislatives,
-      config: app.config
+  render_home = function (legislatives){
+      res.render("home.html", {
+        legislatives: legislatives,
+        config: app.config
+      });
+  };
+  
+  if(district){
+      repo.get_by_district(district, function (err, legislatives) {
+      if (err) {
+        req.db.rollback();
+        return;
+      }
+      render_home(legislatives);
     });
-  });
+  }else{
+      repo.list(function (err, legislatives) {
+      if (err) {
+        req.db.rollback();
+        return;
+      }
+      render_home(legislatives);
+    });    
+  }
 });
-
