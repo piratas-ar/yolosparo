837fb4799b1da0e27d446be9946c7c3d400581d1
Matías MI <matt@spiria.me>
2015-11-02 18:39:59 -0300

    Creates cache directory if it doesn't exist

--- jobs.js
+++ jobs.js
@@ -5,6 +5,10 @@ var path = require("path");
 var jobFiles = fs.readdirSync(path.normalize(JOBS_DIR));
 var CronJob = require('cron').CronJob;
 
+if (!fs.existsSync("data")) {
+  fs.mkdirSync("data");
+}
+
 jobFiles.forEach(file => {
   var cronConfig = require(path.join(JOBS_DIR, file));
   var job = new CronJob(Object.assign({
